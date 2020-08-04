23735abf60595632fedef6193a96a03e3a3fd8da
salatovmi <64735995+salatovmi@users.noreply.github.com>
2020-08-04 16:01:25 +0200

    Create Solution.java

---
+++ java-static-initializer-block/Solution.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+static int B, H;
+    static boolean flag = true;
+
+    static {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+        sc.close();
+        try {
+            if (B < 1 || H < 1) {
+                throw new Exception("Breadth and height must be positive");
+            } 
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(0);
+        }
+    }   
+ public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}		
+	}
+}
