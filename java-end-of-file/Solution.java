8b3e01f19f3c417cba245ceed33a1489675f7a4c
salatovmi <64735995+salatovmi@users.noreply.github.com>
2020-08-04 15:59:30 +0200

    Create Solution.java

---
+++ java-end-of-file/Solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int counter = 1;
+        while (sc.hasNext()) {
+            System.out.println(counter + " " + sc.nextLine());
+            counter++;
+        }
+    }
+}
